5aa7701a3f461d4502a6c50c901c59e7ce466313
buggio <s_artkonghan@hotmail.com>
2016-09-11 10:45:15 +0700

    WebView Success

--- app/src/main/java/snr/artkhonghan/somphet/trafficlearn/MainActivity.java
+++ app/src/main/java/snr/artkhonghan/somphet/trafficlearn/MainActivity.java
@@ -1,13 +1,45 @@
 package snr.artkhonghan.somphet.trafficlearn;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        listView = (ListView) findViewById(R.id.listView);
+        button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.btn_about);
+                mediaPlayer.start();
+
+                // Show Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/v3v0ObzBNvc"));
+                startActivity(intent);
+
+            } // onClick
+        });
+
+
+    }   // Main Method
+
+}   // Main Class
